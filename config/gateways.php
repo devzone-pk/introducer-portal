1a554da065795fdd4ce83a8615ba92e605a7169d
Muhammad Talha <talha@devzone.services>
2023-07-21 14:08:47 +0500

    ok

--- config/gateways.php
+++ config/gateways.php
@@ -4,7 +4,7 @@
 return [
     'trust-payment' => [
         'key' => env('TRUSTPAYMENT_KEY', '60-95500fd7fe4c1c2783ebf66de6084b86fbde620a4b1dff2fa0181786b6ee0327'),
-        'iss' => env('TRUSTPAYMENT_ISS', 'jwt@rozeint.co.uk'),
-        'sitereference' => env('TRUSTPAYMENT_SITE_REF', 'test_rozrint116031')
+        'iss' => env('TRUSTPAYMENT_ISS', 'jwt@remitunion.co.uk'),
+        'sitereference' => env('TRUSTPAYMENT_SITE_REF', 'test_remitunionlimited99788')
     ]
 ];
