fa81ae25549ffd04596d19ad58e203ba446be2ff
Ali <ali2020ahtisham@outlook.com>
2023-08-22 17:36:16 +0500

    update

--- resources/views/mobile/track-transfer.blade.php
+++ resources/views/mobile/track-transfer.blade.php
@@ -6,7 +6,7 @@
 
     <div class="appHeader no-border ">
         <div class="left">
-            <a href="{{ url('mobile/dashboard') }}" class="headerButton">
+            <a href="{{ url('user/welcome') }}" class="headerButton">
                 <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13 14.5L10.5 12L13 9.5" stroke="#FF885B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M8.95043 20.6471C6.17301 19.9956 4.00437 17.827 3.35288 15.0496C2.88237 13.0437 2.88237 10.9563 3.35288 8.95043C4.00437 6.17301 6.17301 4.00437 8.95044 3.35288C10.9563 2.88237 13.0437 2.88238 15.0496 3.35288C17.827 4.00437 19.9956 6.17301 20.6471 8.95043C21.1176 10.9563 21.1176 13.0437 20.6471 15.0496C19.9956 17.827 17.827 19.9956 15.0496 20.6471C13.0437 21.1176 10.9563 21.1176 8.95043 20.6471Z" stroke="#FF885B" stroke-width="1.5"/>
