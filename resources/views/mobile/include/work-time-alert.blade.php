299f6052e63e08a51a2d2e1e0c3dc2c231416c62
Hamza <Hamza22@gmail.com>
2024-01-15 16:19:37 +0500

    d

--- resources/views/mobile/include/work-time-alert.blade.php
+++ resources/views/mobile/include/work-time-alert.blade.php
@@ -3,7 +3,7 @@
         $is_closed = (strtotime($current_time) >= strtotime('09:00') && strtotime($current_time) < strtotime('19:00'));
     @endphp
 
-    @if($is_closed)
+    @if(!$is_closed)
     <div class="section">
     <div class="alert  alert-danger @if($margin) mt-2 @endif mb-2">
         <strong>Attention!</strong> <br>
