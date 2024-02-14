59baabe385ca8566f84729a89c0d01c31708c837
Ali <ali2020ahtisham@outlook.com>
2024-02-14 16:23:50 +0500

    customer rate update

--- app/Models/Customer/CustomerRate.php
+++ app/Models/Customer/CustomerRate.php
@@ -4,8 +4,11 @@ namespace App\Models\Customer;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CustomerRate extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $guarded = [];
 }
