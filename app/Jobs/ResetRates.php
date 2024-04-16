8c1d5dadc309eac3c3f8d5f62dc1292722d9939b
Hamza Mehmood <hamza.mehmood@devzone.pk>
2024-04-16 16:55:47 +0500

    update

---
+++ app/Jobs/ResetRates.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\SourceRate;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ResetRates implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        SourceRate::where('company_id', config('app.company_id'))
+            ->where('rate', '>', 0)
+            ->update([
+                'rate' => 0.0000,
+            ]);
+        
+    }
+}
